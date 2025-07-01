8be4c1253c5a688ff4154ea0b62d7836058021c3
amazonview <chetijasimran@gmail.com>
2025-07-01 18:14:57 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'Disneyplus TV'
+project = 'hulu activate'
 
 
 # The full version, including alpha/beta/rc tags
